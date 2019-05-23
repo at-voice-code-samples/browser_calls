bb054f693dc9ecf2c9947baf59b5d5706a3d603a
Michael Mwirigi <mmwirigi@africastalking.com>
2019-05-23 14:48:38 +0300

    token error bug

--- js/index.js
+++ js/index.js
@@ -14,127 +14,127 @@ function ATlogin() {
         clientName: clientName.value
       })
     })
-      .then(data => { return data.json() })
-      .then(response => {
-        let token = response.token;
-        const at = new Africastalking.Client(token, {
-          // sounds: {
-          //   dialing: 'sounds/dial.mp3',
-          //   ringing: 'sounds/ring.mp3'
-          // }
-        })
-        return at;
+    .then(data => { return data.json() })
+    .then(response => {
+      let token = response.token;
+      const at = new Africastalking.Client(token, {
+        // sounds: {
+        //   dialing: 'sounds/dial.mp3',
+        //   ringing: 'sounds/ring.mp3'
+        // }
       })
-      .then(client => {
-        const logoutBtn = document.getElementById('logout-btn'),
-          hangupBtn = document.getElementById('hangup-btn'),
-          answerBtn = document.getElementById('answer-btn'),
-          callBtn = document.getElementById('call-btn'),
-          callto = document.getElementById('call-to'),
-          outputLabel = document.getElementById('output-lbl'),
-          outputColor = document.getElementById('output-color'),
-          dtmfKeyboard = document.getElementById('dtmf-keyboard');
+      return at;
+    })
+    .then(client => {
+      const logoutBtn = document.getElementById('logout-btn'),
+        hangupBtn = document.getElementById('hangup-btn'),
+        answerBtn = document.getElementById('answer-btn'),
+        callBtn = document.getElementById('call-btn'),
+        callto = document.getElementById('call-to'),
+        outputLabel = document.getElementById('output-lbl'),
+        outputColor = document.getElementById('output-color'),
+        dtmfKeyboard = document.getElementById('dtmf-keyboard');
 
-        logoutBtn.addEventListener("click", function () {
-          client.hangup();
-          client.logout();
+      logoutBtn.addEventListener("click", function () {
+        client.hangup();
+        client.logout();
+      });
+
+      hangupBtn.addEventListener("click", function () {
+        client.hangup();
+      });
+
+      answerBtn.addEventListener("click", function () {
+        client.answer();
+      });
+
+      callBtn.addEventListener("click", function () {
+        let to = document.getElementById('call-to').value;
+        client.call(to, false);
+      });
+
+      for (const key of dtmfKeyboard.querySelectorAll('td')) {
+        key.addEventListener("click", function (events) {
+          const text = events.target.innerHTML;
+          client.dtmf(text);
         });
+      }
 
-        hangupBtn.addEventListener("click", function () {
-          client.hangup();
-        });
+      ////////////////////////webrtc events////////////////////////////
 
-        answerBtn.addEventListener("click", function () {
-          client.answer();
-        });
-
-        callBtn.addEventListener("click", function () {
-          let to = document.getElementById('call-to').value;
-          client.call(to, false);
-        });
-
-        for (const key of dtmfKeyboard.querySelectorAll('td')) {
-          key.addEventListener("click", function (events) {
-            const text = events.target.innerHTML;
-            client.dtmf(text);
-          });
-        }
-
-        ////////////////////////webrtc events////////////////////////////
-
-        client.on('ready', function () {
-          loginBtn.setAttribute('disabled', 'disabled');
-          clientName.setAttribute('disabled', 'disabled');
-          logoutBtn.removeAttribute('disabled');
-          callto.removeAttribute('disabled');
-          callBtn.removeAttribute('disabled');
-          callto.focus();
-          outputColor.classList = 'ui tiny green circular label';
-          outputLabel.textContent = 'Ready to make calls';
-        }, false);
+      client.on('ready', function () {
+        loginBtn.setAttribute('disabled', 'disabled');
+        clientName.setAttribute('disabled', 'disabled');
+        logoutBtn.removeAttribute('disabled');
+        callto.removeAttribute('disabled');
+        callBtn.removeAttribute('disabled');
+        callto.focus();
+        outputColor.classList = 'ui tiny green circular label';
+        outputLabel.textContent = 'Ready to make calls';
+      }, false);
 
 
-        client.on('notready', function () {
-          loginBtn.removeAttribute('disabled');
-          clientName.removeAttribute('disabled');
-          logoutBtn.setAttribute('disabled', 'disabled');
-          callto.setAttribute('disabled', 'disabled');
-          callBtn.setAttribute('disabled', 'disabled');
-          outputLabel.textContent = 'Login';
-          outputColor.classList = 'ui tiny orange circular label'
-        }, false);
+      client.on('notready', function () {
+        loginBtn.removeAttribute('disabled');
+        clientName.removeAttribute('disabled');
+        logoutBtn.setAttribute('disabled', 'disabled');
+        callto.setAttribute('disabled', 'disabled');
+        callBtn.setAttribute('disabled', 'disabled');
+        outputLabel.textContent = 'Login';
+        outputColor.classList = 'ui tiny orange circular label'
+      }, false);
 
-        client.on('calling', function () {
-          hangupBtn.removeAttribute('disabled');
-          callto.setAttribute('disabled', 'disabled');
-          callBtn.setAttribute('disabled', 'disabled');
-          outputLabel.textContent = 'Calling ' + client.getCounterpartNum() + '...';
-          outputColor.classList = 'ui tiny green circular label'
-        }, false);
+      client.on('calling', function () {
+        hangupBtn.removeAttribute('disabled');
+        callto.setAttribute('disabled', 'disabled');
+        callBtn.setAttribute('disabled', 'disabled');
+        outputLabel.textContent = 'Calling ' + client.getCounterpartNum() + '...';
+        outputColor.classList = 'ui tiny green circular label'
+      }, false);
 
-        client.on('incomingcall', function (params) {
-          hangupBtn.removeAttribute('disabled');
-          answerBtn.removeAttribute('disabled');
-          callBtn.setAttribute('disabled', 'disabled');
-          callto.setAttribute('disabled', 'disabled');
-          outputLabel.textContent = '...incoming call from ' + params.from;
-          outputColor.classList = 'ui tiny green circular label'
-        }, false);
+      client.on('incomingcall', function (params) {
+        hangupBtn.removeAttribute('disabled');
+        answerBtn.removeAttribute('disabled');
+        callBtn.setAttribute('disabled', 'disabled');
+        callto.setAttribute('disabled', 'disabled');
+        outputLabel.textContent = '...incoming call from ' + params.from;
+        outputColor.classList = 'ui tiny green circular label'
+      }, false);
 
-        client.on('callaccepted', function () {
-          hangupBtn.removeAttribute('disabled');
-          callBtn.setAttribute('disabled', 'disabled');
-          callto.setAttribute('disabled', 'disabled');
-          answerBtn.setAttribute('disabled', 'disabled');
-          dtmfKeyboard.style.display = 'initial';
-          outputLabel.textContent = 'In conversation with ' + client.getCounterpartNum();
-          outputColor.classList = 'ui tiny green circular label'
-        }, false);
-
-
-        client.on('hangup', function () {
-          hangupBtn.setAttribute('disabled', 'disabled');
-          answerBtn.setAttribute('disabled', 'disabled');
-          callBtn.removeAttribute('disabled');
-          callto.removeAttribute('disabled');
-          dtmfKeyboard.style.display = 'none';
-          outputLabel.textContent = 'Registered';
-          outputColor.classList = 'ui tiny orange circular label'
-        }, false);
+      client.on('callaccepted', function () {
+        hangupBtn.removeAttribute('disabled');
+        callBtn.setAttribute('disabled', 'disabled');
+        callto.setAttribute('disabled', 'disabled');
+        answerBtn.setAttribute('disabled', 'disabled');
+        dtmfKeyboard.style.display = 'initial';
+        outputLabel.textContent = 'In conversation with ' + client.getCounterpartNum();
+        outputColor.classList = 'ui tiny green circular label'
+      }, false);
 
 
-        //////////////////////add this
-        client.on('offline', function () {
-          outputLabel.textContent = 'Token expired, refresh page';
-          outputColor.classList = 'ui tiny red circular label'
-        }, false);
+      client.on('hangup', function () {
+        hangupBtn.setAttribute('disabled', 'disabled');
+        answerBtn.setAttribute('disabled', 'disabled');
+        callBtn.removeAttribute('disabled');
+        callto.removeAttribute('disabled');
+        dtmfKeyboard.style.display = 'none';
+        outputLabel.textContent = 'Registered';
+        outputColor.classList = 'ui tiny orange circular label'
+      }, false);
 
-        client.on('closed', function () {
-          outputLabel.textContent = 'connection closed, refresh page';
-          outputColor.classList = 'ui tiny red circular label'
-        }, false);
-      })
-      .catch(error => console.log(error));
+
+      //////////////////////add this
+      client.on('offline', function () {
+        outputLabel.textContent = 'Token expired, refresh page';
+        outputColor.classList = 'ui tiny red circular label'
+      }, false);
+
+      client.on('closed', function () {
+        outputLabel.textContent = 'connection closed, refresh page';
+        outputColor.classList = 'ui tiny red circular label'
+      }, false);
+    })
+    .catch(error => console.log(error));
   }
   else {
     clientName.value = 'make sure client name is valid';
