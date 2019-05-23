96ff3a8a1e4d04c93a1088aa30d5cf3b8f5b8a90
Michael Mwirigi <mmwirigi@africastalking.com>
2019-05-23 13:04:54 +0300

    layout edits

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const router = express.Router();
 const axios = require('axios');
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
