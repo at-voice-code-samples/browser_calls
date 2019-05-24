07f815181e73bcd6881acee5f942bc019674f5fd
Michael Mwirigi <mmwirigi@africastalking.com>
2019-05-24 12:25:33 +0300

    updated ui and  removing username

--- js/index.js
+++ js/index.js
@@ -7,134 +7,135 @@ loginBtn.addEventListener("click", function () {
 function ATlogin() {
   const clientName = document.getElementById('client-name');
   if (!(/\s/.test(clientName.value))) {
-    fetch('/capability-token', {
+    fetch('http://localhost:5000/capability-token', {
       headers: { "Content-Type": "application/json; charset=utf-8" },
       method: 'POST',
       body: JSON.stringify({
         clientName: clientName.value
       })
     })
-    .then(data => { return data.json() })
-    .then(response => {
-      let token = response.token;
-      const at = new Africastalking.Client(token, {
-        // sounds: {
-        //   dialing: 'sounds/dial.mp3',
-        //   ringing: 'sounds/ring.mp3'
-        // }
+      .then(data => { return data.json() })
+      .then(response => {
+        let token = response.token;
+        const at = new Africastalking.Client(token, {
+          sounds: {
+            dialing: '/sounds/dial.mp3',
+            ringing: '/sounds/ring.mp3'
+          }
+        })
+        return at;
       })
-      return at;
-    })
-    .then(client => {
-      const logoutBtn = document.getElementById('logout-btn'),
-        hangupBtn = document.getElementById('hangup-btn'),
-        answerBtn = document.getElementById('answer-btn'),
-        callBtn = document.getElementById('call-btn'),
-        callto = document.getElementById('call-to'),
-        outputLabel = document.getElementById('output-lbl'),
-        outputColor = document.getElementById('output-color'),
-        dtmfKeyboard = document.getElementById('dtmf-keyboard');
+      .then(client => {
+        const logoutBtn = document.getElementById('logout-btn'),
+          hangupBtn = document.getElementById('hangup-btn'),
+          answerBtn = document.getElementById('answer-btn'),
+          callBtn = document.getElementById('call-btn'),
+          callto = document.getElementById('call-to'),
+          outputLabel = document.getElementById('output-lbl'),
+          outputColor = document.getElementById('output-color'),
+          dtmfKeyboard = document.getElementById('dtmf-keyboard');
 
-      logoutBtn.addEventListener("click", function () {
-        client.hangup();
-        client.logout();
-      });
-
-      hangupBtn.addEventListener("click", function () {
-        client.hangup();
-      });
-
-      answerBtn.addEventListener("click", function () {
-        client.answer();
-      });
-
-      callBtn.addEventListener("click", function () {
-        let to = document.getElementById('call-to').value;
-        client.call(to, false);
-      });
-
-      for (const key of dtmfKeyboard.querySelectorAll('td')) {
-        key.addEventListener("click", function (events) {
-          const text = events.target.innerHTML;
-          client.dtmf(text);
+        logoutBtn.addEventListener("click", function () {
+          client.hangup();
+          client.logout();
         });
-      }
 
-      ////////////////////////webrtc events////////////////////////////
+        hangupBtn.addEventListener("click", function () {
+          client.hangup();
+        });
 
-      client.on('ready', function () {
-        loginBtn.setAttribute('disabled', 'disabled');
-        clientName.setAttribute('disabled', 'disabled');
-        logoutBtn.removeAttribute('disabled');
-        callto.removeAttribute('disabled');
-        callBtn.removeAttribute('disabled');
-        callto.focus();
-        outputColor.classList = 'ui tiny green circular label';
-        outputLabel.textContent = 'Ready to make calls';
-      }, false);
+        answerBtn.addEventListener("click", function () {
+          client.answer();
+        });
 
+        callBtn.addEventListener("click", function () {
+          let to = document.getElementById('call-to').value;
+          if (/^[a-zA-Z]+/.test(to)) { to = 'mwirigiApp.'+ to }
+          client.call(to, false);
+        });
 
-      client.on('notready', function () {
-        loginBtn.removeAttribute('disabled');
-        clientName.removeAttribute('disabled');
-        logoutBtn.setAttribute('disabled', 'disabled');
-        callto.setAttribute('disabled', 'disabled');
-        callBtn.setAttribute('disabled', 'disabled');
-        outputLabel.textContent = 'Login';
-        outputColor.classList = 'ui tiny orange circular label'
-      }, false);
+        for (const key of dtmfKeyboard.querySelectorAll('td')) {
+          key.addEventListener("click", function (events) {
+            const text = events.target.innerHTML;
+            client.dtmf(text);
+          });
+        }
 
-      client.on('calling', function () {
-        hangupBtn.removeAttribute('disabled');
-        callto.setAttribute('disabled', 'disabled');
-        callBtn.setAttribute('disabled', 'disabled');
-        outputLabel.textContent = 'Calling ' + client.getCounterpartNum() + '...';
-        outputColor.classList = 'ui tiny green circular label'
-      }, false);
+        ////////////////////////webrtc events////////////////////////////
 
-      client.on('incomingcall', function (params) {
-        hangupBtn.removeAttribute('disabled');
-        answerBtn.removeAttribute('disabled');
-        callBtn.setAttribute('disabled', 'disabled');
-        callto.setAttribute('disabled', 'disabled');
-        outputLabel.textContent = '...incoming call from ' + params.from;
-        outputColor.classList = 'ui tiny green circular label'
-      }, false);
-
-      client.on('callaccepted', function () {
-        hangupBtn.removeAttribute('disabled');
-        callBtn.setAttribute('disabled', 'disabled');
-        callto.setAttribute('disabled', 'disabled');
-        answerBtn.setAttribute('disabled', 'disabled');
-        dtmfKeyboard.style.display = 'initial';
-        outputLabel.textContent = 'In conversation with ' + client.getCounterpartNum();
-        outputColor.classList = 'ui tiny green circular label'
-      }, false);
+        client.on('ready', function () {
+          loginBtn.setAttribute('disabled', 'disabled');
+          clientName.setAttribute('disabled', 'disabled');
+          logoutBtn.removeAttribute('disabled');
+          callto.removeAttribute('disabled');
+          callBtn.removeAttribute('disabled');
+          callto.focus();
+          outputColor.classList = 'ui tiny green circular label';
+          outputLabel.textContent = 'Ready to make calls';
+        }, false);
 
 
-      client.on('hangup', function () {
-        hangupBtn.setAttribute('disabled', 'disabled');
-        answerBtn.setAttribute('disabled', 'disabled');
-        callBtn.removeAttribute('disabled');
-        callto.removeAttribute('disabled');
-        dtmfKeyboard.style.display = 'none';
-        outputLabel.textContent = 'Registered';
-        outputColor.classList = 'ui tiny orange circular label'
-      }, false);
+        client.on('notready', function () {
+          loginBtn.removeAttribute('disabled');
+          clientName.removeAttribute('disabled');
+          logoutBtn.setAttribute('disabled', 'disabled');
+          callto.setAttribute('disabled', 'disabled');
+          callBtn.setAttribute('disabled', 'disabled');
+          outputLabel.textContent = 'Login';
+          outputColor.classList = 'ui tiny orange circular label'
+        }, false);
+
+        client.on('calling', function () {
+          hangupBtn.removeAttribute('disabled');
+          callto.setAttribute('disabled', 'disabled');
+          callBtn.setAttribute('disabled', 'disabled');
+          outputLabel.textContent = 'Calling ' + client.getCounterpartNum().replace("mwirigiApp.", "") + '...';
+          outputColor.classList = 'ui tiny green circular label'
+        }, false);
+
+        client.on('incomingcall', function (params) {
+          hangupBtn.removeAttribute('disabled');
+          answerBtn.removeAttribute('disabled');
+          callBtn.setAttribute('disabled', 'disabled');
+          callto.setAttribute('disabled', 'disabled');
+          outputLabel.textContent = 'Incoming call from ' + params.from.replace("mwirigiApp.", "");
+          outputColor.classList = 'ui tiny green circular label'
+        }, false);
+
+        client.on('callaccepted', function () {
+          hangupBtn.removeAttribute('disabled');
+          callBtn.setAttribute('disabled', 'disabled');
+          callto.setAttribute('disabled', 'disabled');
+          answerBtn.setAttribute('disabled', 'disabled');
+          dtmfKeyboard.style.display = 'initial';
+          outputLabel.textContent = 'In conversation with ' + client.getCounterpartNum().replace("mwirigiApp.", "");
+          outputColor.classList = 'ui tiny green circular label'
+        }, false);
 
 
-      //////////////////////add this
-      client.on('offline', function () {
-        outputLabel.textContent = 'Token expired, refresh page';
-        outputColor.classList = 'ui tiny red circular label'
-      }, false);
+        client.on('hangup', function () {
+          hangupBtn.setAttribute('disabled', 'disabled');
+          answerBtn.setAttribute('disabled', 'disabled');
+          callBtn.removeAttribute('disabled');
+          callto.removeAttribute('disabled');
+          dtmfKeyboard.style.display = 'none';
+          outputLabel.textContent = 'Call ended';
+          outputColor.classList = 'ui tiny orange circular label'
+        }, false);
 
-      client.on('closed', function () {
-        outputLabel.textContent = 'connection closed, refresh page';
-        outputColor.classList = 'ui tiny red circular label'
-      }, false);
-    })
-    .catch(error => console.log(error));
+
+        //////////////////////add this
+        client.on('offline', function () {
+          outputLabel.textContent = 'Token expired, refresh page';
+          outputColor.classList = 'ui tiny red circular label'
+        }, false);
+
+        client.on('closed', function () {
+          outputLabel.textContent = 'connection closed, refresh page';
+          outputColor.classList = 'ui tiny red circular label'
+        }, false);
+      })
+      .catch(error => console.log(error));
   }
   else {
     clientName.value = 'make sure client name is valid';
