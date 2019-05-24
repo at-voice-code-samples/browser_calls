3f6d19d82117a9562f7fe6bde7b694b9ffcde836
Michael Mwirigi <mmwirigi@africastalking.com>
2019-05-24 13:54:30 +0300

    commit

--- app.js
+++ app.js
@@ -51,11 +51,31 @@ app.post('/webrtc', (req, res) => {
     console.log(JSON.stringify(req.body, null, 2));
     const clientDialedNumber = req.body.clientDialedNumber;
     if (req.body.clientDialedNumber){
-        callActions = `<Dial phoneNumbers="${clientDialedNumber}"/>`;
+    	// callActions = `<Dial phoneNumbers="${clientDialedNumber}"/>`;
+        callActions = `<GetDigits timeout="30" finishOnKey="#" callbackUrl="https://at-voice.herokuapp.com/dtmf">
+				        <Say>Press one then hash to continue or press two then hash to listen to the voice team mixtape</Say>
+				    </GetDigits>`;
+
     }
     else {
         callActions = `<Dial phoneNumbers="${lastRegisteredClient}"/>`;   
     }
+    responseAction = '<?xml version="1.0" encoding="UTF-8"?><Response>' + `${callActions}` + '</Response>';
+    res.send(responseAction);
+});
+
+
+app.post('/dtmf', (req, res) => {
+    console.log(JSON.stringify(req.body, null, 2));
+    const client = req.body;
+    if (client.dtmfDigits === '1'){
+    	callActions = `<Dial phoneNumbers="${client.clientDialedNumber}"/>`;
+	} else if (client.dtmfDigits === '2'){
+		callActions = `<Play url="https://at-voice.herokuapp.com/sounds/babyshark.mp3"/>`;
+	}
+	else{
+		callActions = `<Say>Wrong input added</Say>`;
+	}
     responseAction = '<?xml version="1.0" encoding="UTF-8"?><Response>' + `${callActions}` + '</Response>';
     res.send(responseAction);
 });
